c9fef391e82b840b991b74d31b4fda430b2e15de
ScreamerFox <142076193+ScreamerFox@users.noreply.github.com>
2024-07-24 20:18:36 +0500

    module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -1,20 +1,16 @@
-class House():                           # создаём класс
+class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def go_to(self, new_floor):  # Доставляем на этаж
-        for i in range(1, new_floor + 1):
-            if new_floor < 1:
-                print("такого этажа не существует")
-                break
-            elif new_floor > self.number_of_floors:
-                print("такого этажа тоже не существует")
-                break
-            else:
-                print(f'Вы прибыли на {i} этаж!')
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
 
-house1 = House("Андреевский", 10)   # создаём 2 дома
-house2 = House("Cleopatra", 2)
+house1 = House("Андреевский", 5)   # создаём 2 дома
+house2 = House("Cleopatra", 10)
 house1.go_to(11)
 house2.go_to(2)
