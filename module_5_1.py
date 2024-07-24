d919033413810b4b8ae0ab57fe90e5893f3bc5a8
ScreamerFox <142076193+ScreamerFox@users.noreply.github.com>
2024-07-24 20:14:27 +0500

    module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -4,27 +4,17 @@ class House():                           # создаём класс
         self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):  # Доставляем на этаж
-        if new_floor < 1:
-            print("такого этажа не существует")
-        elif new_floor > self.number_of_floors:
-            print("такого этажа тоже не существует")
-        else:
-            print(f'Вы прибыли на {new_floor} этаж!')
+        for i in range(1, new_floor + 1):
+            if new_floor < 1:
+                print("такого этажа не существует")
+                break
+            elif new_floor > self.number_of_floors:
+                print("такого этажа тоже не существует")
+                break
+            else:
+                print(f'Вы прибыли на {i} этаж!')
 
 house1 = House("Андреевский", 10)   # создаём 2 дома
-house2 = House("Cleopatra", 521)
-
-money1 = input('Здравствуйте, у вас больше 1000000$? да/нет: ')     # запрашиваем достаток клиента
-
-def choice_house(money):                                            # Выбираем дом и этаж
-    if money.lower() == 'да':
-        lift2 = int(input('Добро пожаловать в элитный жк Cleopatra, на какой этаж хотите попасть?'))
-        House.go_to(house2, lift2)
-    else:
-        lift = int(input('Добро пожаловать в жк Андреевский, на какой этаж хотите попасть?'))
-        House.go_to(house1, lift)
-
-
-
-
-choice_house(money1)
+house2 = House("Cleopatra", 2)
+house1.go_to(11)
+house2.go_to(2)
